56dc1045aa8668ddc4e9c6ecf74869685416f6ed
Ramiz Hida <ramizhida00@gmail.com>
2023-01-05 13:01:52 +0100

    Submit a Note

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -5,7 +5,11 @@ import Spinner from '../components/Spinner';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import {
+  getNotes,
+  createNote,
+  reset as notesReset,
+} from '../features/notes/noteSlice';
 import NoteItem from '../components/NoteItem';
 import Modal from 'react-modal';
 import { FaPlus } from 'react-icons/fa';
@@ -61,7 +65,7 @@ function Ticket() {
   // Create note submit
   const onNoteSubmit = (e) => {
     e.preventDefault();
-    console.log('Submit');
+    dispatch(createNote({ noteText, ticketId }));
     closeModal();
   };
 
